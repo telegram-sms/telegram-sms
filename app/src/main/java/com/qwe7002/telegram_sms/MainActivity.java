3a90b92bb3b6bfab4558604083287a7ad28ee8c3
qwe7002 <qwe7002@hotmail.com>
2018-10-01 19:01:44 +0800

    移除自动填充功能

--- app/src/main/java/com/qwe7002/telegram_sms/MainActivity.java
+++ app/src/main/java/com/qwe7002/telegram_sms/MainActivity.java
@@ -9,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Set up the login form.
     }
 
 }
