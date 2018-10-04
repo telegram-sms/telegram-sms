db5aa6ea7754c48ff59e50a7e4c30358df3f486a
qwe7002 <qwe7002@hotmail.com>
2018-10-04 09:24:03 +0800

    添加电源检测

--- app/src/main/java/com/qwe7002/telegram_sms/boot_receiver.java
+++ app/src/main/java/com/qwe7002/telegram_sms/boot_receiver.java
@@ -3,6 +3,7 @@ package com.qwe7002.telegram_sms;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 
 import java.util.Objects;
@@ -12,9 +13,13 @@ public class boot_receiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Objects.equals(intent.getAction(), "android.intent.action.BOOT_COMPLETED")) {
-            Log.d("tg-sms", "onReceive: boot-completed");
+            Log.d("tg-sms", "onReceive: boot_completed");
             Intent service = new Intent(context, registry_service.class);
-            context.startService(service);
-        }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(service);
+            } else {
+                context.startService(service);
+            }
         }
     }
+}
--- app/src/main/java/com/qwe7002/telegram_sms/registry_service.java
+++ app/src/main/java/com/qwe7002/telegram_sms/registry_service.java
@@ -1,23 +1,51 @@
 package com.qwe7002.telegram_sms;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.IBinder;
+import android.util.Log;
 
 public class registry_service extends Service {
-    public registry_service() {
+    battery_receiver receiver = null;
+    final String CHANNEL_ID = "1";
+    final String CHANNEL_NAME="tg-sms";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("tg-sms", "onCreate: battery_receiver");
+        battery_receiver receiver = new battery_receiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_BATTERY_LOW);
+        registerReceiver(receiver, filter);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_MIN);
+            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.createNotificationChannel(channel);
+
+            Notification notification = new Notification.Builder(getApplicationContext(), CHANNEL_ID).build();
+            startForeground(1, notification);
+        }
+
+
+
     }
 
     @Override
-    public void onCreate() {
-        battery_receiver receiver=new battery_receiver();
-        IntentFilter filter=new IntentFilter(Intent.ACTION_BATTERY_LOW);
-        registerReceiver(receiver, filter);
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
+
 }
