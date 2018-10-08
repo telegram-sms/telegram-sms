79a00309e488e1dbd1601f208d72553de14f6c92
qwe7002 <qwe7002@hotmail.com>
2018-10-08 20:43:28 +0800

    修改电池广播位置

--- app/src/main/java/com/qwe7002/telegram_sms/registry_service.java
+++ app/src/main/java/com/qwe7002/telegram_sms/registry_service.java
@@ -4,12 +4,31 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.IBinder;
+import android.os.Looper;
+import android.support.annotation.NonNull;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class registry_service extends Service {
     battery_receiver receiver = null;
@@ -48,4 +67,62 @@ public class registry_service extends Service {
         return null;
     }
 
+}
+
+class battery_receiver extends BroadcastReceiver {
+    static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("data", MODE_PRIVATE);
+        Log.d("tg-sms", "onReceive: battery_low");
+        String bot_token = sharedPreferences.getString("bot_token", "");
+        String chat_id = sharedPreferences.getString("chat_id", "");
+        String request_uri = "https://api.telegram.org/bot" + bot_token + "/sendMessage";
+        if (bot_token.isEmpty() || chat_id.isEmpty()) {
+            Log.i("tg-sms", "onReceive: token not found");
+            return;
+        }
+        request_json request_body = new request_json();
+        request_body.chat_id = chat_id;
+        switch (intent.getAction()) {
+            case Intent.ACTION_BATTERY_LOW:
+                request_body.text = "System message\nDevice battery is low.";
+                break;
+            case Intent.ACTION_POWER_CONNECTED:
+                request_body.text = "System message\nAC Charger connected.";
+                break;
+            case Intent.ACTION_POWER_DISCONNECTED:
+                request_body.text = "System message\nAC Charger disconnected.";
+                break;
+        }
+
+
+        Gson gson = new Gson();
+        String request_body_raw = gson.toJson(request_body);
+        RequestBody body = RequestBody.create(JSON, request_body_raw);
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder().url(request_uri).method("POST", body).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Looper.prepare();
+                Toast.makeText(context, "SendSMSError:" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                Looper.loop();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.code() != 200) {
+                    Looper.prepare();
+                    assert response.body() != null;
+                    Toast.makeText(context, "SendSMSError:" + response.body().string(), Toast.LENGTH_SHORT).show();
+                    Looper.loop();
+                }
+            }
+        });
+
+
+    }
 }
