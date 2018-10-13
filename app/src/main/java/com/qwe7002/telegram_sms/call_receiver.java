14eb3245fbdf787adcb3a30594e0257f91c80e61
qwe7002 <qwe7002@hotmail.com>
2018-10-14 03:22:11 +0800

    添加双卡支持

--- app/src/main/java/com/qwe7002/telegram_sms/call_receiver.java
+++ app/src/main/java/com/qwe7002/telegram_sms/call_receiver.java
@@ -1,18 +1,23 @@
 package com.qwe7002.telegram_sms;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Looper;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.telephony.PhoneStateListener;
+import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -27,10 +32,10 @@ import static android.content.Context.MODE_PRIVATE;
 public class call_receiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals("android.intent.action.PHONE_STATE")) {
+        if (Objects.equals(intent.getAction(), "android.intent.action.PHONE_STATE")) {
             TelephonyManager telephony = (TelephonyManager) context
                     .getSystemService(Context.TELEPHONY_SERVICE);
-            call_listener customPhoneListener = new call_listener(context);
+            call_listener customPhoneListener = new call_listener(context, intent);
             telephony.listen(customPhoneListener,
                     PhoneStateListener.LISTEN_CALL_STATE);
         }
@@ -41,30 +46,39 @@ class call_listener extends PhoneStateListener {
     private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
     private static int lastState = TelephonyManager.CALL_STATE_IDLE;
     private Context context;
+    private Intent intent;
 
-    call_listener(Context context) {
+    call_listener(Context context, Intent intent) {
         super();
         this.context = context;
+        this.intent = intent;
     }
 
     public void onCallStateChanged(int state, String incomingNumber) {
         if (lastState == TelephonyManager.CALL_STATE_RINGING
                 && state == TelephonyManager.CALL_STATE_IDLE) {
-            sendSmgWhenMissedCall(incomingNumber);
+            int slot = Integer.parseInt((String) Objects.requireNonNull(Objects.requireNonNull(intent.getExtras()).get("slot")));
+            sendSmgWhenMissedCall(incomingNumber, slot);
         }
 
         lastState = state;
     }
 
-    private void sendSmgWhenMissedCall(String incomingNumber) {
+    private void sendSmgWhenMissedCall(String incomingNumber, int simslot) {
         SharedPreferences sharedPreferences = context.getSharedPreferences("data", MODE_PRIVATE);
-
+        SubscriptionManager manager = SubscriptionManager.from(context);
+        String DualSim = "";
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            if (manager.getActiveSubscriptionInfoCount() == 2) {
+                DualSim = "\n" + context.getString(R.string.SIM_card_slot) + simslot;
+            }
+        }
         String bot_token = sharedPreferences.getString("bot_token", "");
         String chat_id = sharedPreferences.getString("chat_id", "");
         String request_uri = "https://api.telegram.org/bot" + bot_token + "/sendMessage";
         request_json request_body = new request_json();
         request_body.chat_id = chat_id;
-        request_body.text = context.getString(R.string.missed_call_head) + "\n" + context.getString(R.string.incoming_numbler) + incomingNumber;
+        request_body.text = context.getString(R.string.missed_call_head) + DualSim + "\n" + context.getString(R.string.incoming_numbler) + incomingNumber;
         Gson gson = new Gson();
         String request_body_raw = gson.toJson(request_body);
         RequestBody body = RequestBody.create(JSON, request_body_raw);
