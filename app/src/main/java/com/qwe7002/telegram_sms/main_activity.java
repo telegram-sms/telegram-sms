e7f4f8088631f7e70d50acd19f68d0a4b6faf70f
qwe7002 <qwe7002@hotmail.com>
2018-11-07 17:01:57 +0800

    恢复原地址

--- app/src/main/java/com/qwe7002/telegram_sms/main_activity.java
+++ app/src/main/java/com/qwe7002/telegram_sms/main_activity.java
@@ -14,7 +14,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
