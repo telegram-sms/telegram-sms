fcfaecf026c9de8f9bf194aca10c2f5b448e6446
qwe7002 <qwe7002@hotmail.com>
2018-11-18 19:55:04 +0800

    修改名称标准

--- app/src/main/java/com/qwe7002/telegram_sms/webhook_service.java
+++ app/src/main/java/com/qwe7002/telegram_sms/webhook_service.java
@@ -226,9 +226,9 @@ public class webhook_service extends Service {
                 Gson gson = new Gson();
                 String request_body_raw = gson.toJson(request_body);
                 RequestBody body = RequestBody.create(public_func.JSON, request_body_raw);
-                OkHttpClient okHttpClient = public_func.get_okhttp_obj();
+                OkHttpClient okhttp_client = public_func.get_okhttp_obj();
                 Request send_request = new Request.Builder().url(request_uri).method("POST", body).build();
-                Call call = okHttpClient.newCall(send_request);
+                Call call = okhttp_client.newCall(send_request);
                 call.enqueue(new Callback() {
                     @Override
                     public void onFailure(@NonNull Call call, @NonNull IOException e) {
