9a7e8154c4415f47805fa74917d5ba55478e6001
qwe7002 <qwe7002@hotmail.com>
2019-03-18 18:16:19 +0800

    回退到异步

--- app/src/main/java/com/qwe7002/telegram_sms/chat_long_polling_service.java
+++ app/src/main/java/com/qwe7002/telegram_sms/chat_long_polling_service.java
@@ -11,6 +11,7 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.BatteryManager;
 import android.os.IBinder;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 
 import com.google.gson.Gson;
@@ -24,6 +25,7 @@ import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -294,16 +296,22 @@ public class chat_long_polling_service extends Service {
         RequestBody body = RequestBody.create(public_func.JSON, gson.toJson(request_body));
         Request send_request = new Request.Builder().url(request_uri).method("POST", body).build();
         Call call = okhttp_client.newCall(send_request);
-        try {
-            Response response = call.execute();
-            if (response.code() != 200) {
-                assert response.body() != null;
-                public_func.write_log(context, "Send reply failed:" + response.body().string());
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                String error_message = "Send reply failed:" + e.getMessage();
+                public_func.write_log(context, error_message);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-            public_func.write_log(context, "Send reply failed:" + e.getMessage());
-        }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.code() != 200) {
+                    assert response.body() != null;
+                    String error_message = "Send reply failed:" + response.body().string();
+                    public_func.write_log(context, error_message);
+                }
+            }
+        });
     }
 
     class stop_broadcast_receiver extends BroadcastReceiver {
