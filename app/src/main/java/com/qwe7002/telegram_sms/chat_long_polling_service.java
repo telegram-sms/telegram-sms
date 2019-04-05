8769d1b7cedb1a4b73a6fccb77efef077470bbcf
qwe7002 <qwe7002@hotmail.com>
2019-04-05 19:50:00 +0800

    修改不合理超时设置

--- app/src/main/java/com/qwe7002/telegram_sms/chat_long_polling_service.java
+++ app/src/main/java/com/qwe7002/telegram_sms/chat_long_polling_service.java
@@ -86,7 +86,7 @@ public class chat_long_polling_service extends Service {
 
 
     void start_long_polling() {
-        int read_timeout = 30 * magnification;
+        int read_timeout = 5 * magnification;
         OkHttpClient okhttp_client_new = okhttp_client.newBuilder()
                 .readTimeout((read_timeout + 5), TimeUnit.SECONDS)
                 .writeTimeout((read_timeout + 5), TimeUnit.SECONDS)
@@ -138,7 +138,7 @@ public class chat_long_polling_service extends Service {
                     receive_handle(item.getAsJsonObject());
                 }
             }
-            if (magnification <= 9) {
+            if (magnification <= 11) {
                 magnification++;
             }
         }
