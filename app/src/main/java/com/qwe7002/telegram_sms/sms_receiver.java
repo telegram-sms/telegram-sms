06e243b7c5206685a13360ca0dbe9f85c3389203
qwe7002 <qwe7002@hotmail.com>
2019-05-25 21:37:35 +0800

    修改receive

--- app/src/main/java/com/qwe7002/telegram_sms/sms_receiver.java
+++ app/src/main/java/com/qwe7002/telegram_sms/sms_receiver.java
@@ -64,92 +64,96 @@ public class sms_receiver extends BroadcastReceiver {
             }
 
         }
-        if (messages.length > 0) {
-            StringBuilder message_body = new StringBuilder();
-            for (SmsMessage item : messages) {
-                message_body.append(item.getMessageBody());
-            }
-            String message_address = messages[0].getOriginatingAddress();
+        if (messages.length == 0) {
+            public_func.write_log(context, "Message length is equal to 0");
+            return;
+        }
+        StringBuilder message_body = new StringBuilder();
+        for (SmsMessage item : messages) {
+            message_body.append(item.getMessageBody());
+        }
+        String message_address = messages[0].getOriginatingAddress();
 
-            final message_json request_body = new message_json();
-            request_body.chat_id = chat_id;
-            String display_address = message_address;
-            if (display_address != null) {
-                String display_name = public_func.get_contact_name(context, display_address);
-                if (display_name != null) {
-                    display_address = display_name + "(" + display_address + ")";
-                }
+        final message_json request_body = new message_json();
+        request_body.chat_id = chat_id;
+        String display_address = message_address;
+        if (display_address != null) {
+            String display_name = public_func.get_contact_name(context, display_address);
+            if (display_name != null) {
+                display_address = display_name + "(" + display_address + ")";
             }
-            request_body.text = "[" + dual_sim + context.getString(R.string.receive_sms_head) + "]" + "\n" + context.getString(R.string.from) + display_address + "\n" + context.getString(R.string.content) + message_body;
-            assert message_address != null;
-            if (android.support.v4.content.ContextCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
-                if (message_address.equals(sharedPreferences.getString("trusted_phone_number", null))) {
-                    String[] msg_send_list = message_body.toString().split("\n");
-                    String msg_send_to = public_func.get_send_phone_number(msg_send_list[0]);
-                    if (message_body.toString().equals("restart-service")) {
-                        public_func.stop_all_service(context.getApplicationContext());
-                        public_func.start_service(context.getApplicationContext(), sharedPreferences.getBoolean("battery_monitoring_switch", false), sharedPreferences.getBoolean("chat_command", false));
-                        request_body.text = context.getString(R.string.system_message_head) + "\n" + context.getString(R.string.restart_service);
-                    }
-                    if (public_func.is_numeric(msg_send_to) && msg_send_list.length != 1) {
-                        StringBuilder msg_send_content = new StringBuilder();
-                        for (int i = 1; i < msg_send_list.length; i++) {
-                            if (msg_send_list.length != 2 && i != 1) {
-                                msg_send_content.append("\n");
-                            }
-                            msg_send_content.append(msg_send_list[i]);
+        }
+        request_body.text = "[" + dual_sim + context.getString(R.string.receive_sms_head) + "]" + "\n" + context.getString(R.string.from) + display_address + "\n" + context.getString(R.string.content) + message_body;
+        assert message_address != null;
+        if (android.support.v4.content.ContextCompat.checkSelfPermission(context, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+            if (message_address.equals(sharedPreferences.getString("trusted_phone_number", null))) {
+                String[] msg_send_list = message_body.toString().split("\n");
+                String msg_send_to = public_func.get_send_phone_number(msg_send_list[0]);
+                if (message_body.toString().equals("restart-service")) {
+                    public_func.stop_all_service(context.getApplicationContext());
+                    public_func.start_service(context.getApplicationContext(), sharedPreferences.getBoolean("battery_monitoring_switch", false), sharedPreferences.getBoolean("chat_command", false));
+                    request_body.text = context.getString(R.string.system_message_head) + "\n" + context.getString(R.string.restart_service);
+                }
+                if (public_func.is_numeric(msg_send_to) && msg_send_list.length != 1) {
+                    StringBuilder msg_send_content = new StringBuilder();
+                    for (int i = 1; i < msg_send_list.length; i++) {
+                        if (msg_send_list.length != 2 && i != 1) {
+                            msg_send_content.append("\n");
                         }
-                        new Thread(() -> public_func.send_sms(context, msg_send_to, msg_send_content.toString(), slot, sub)).start();
-                        return;
+                        msg_send_content.append(msg_send_list[i]);
                     }
+                    new Thread(() -> public_func.send_sms(context, msg_send_to, msg_send_content.toString(), slot, sub)).start();
+                    return;
                 }
             }
-            if (!public_func.check_network(context)) {
-                public_func.write_log(context, public_func.network_error);
-                public_func.send_fallback_sms(context, request_body.text, sub);
-                return;
+        }
+        if (!public_func.check_network(context)) {
+            public_func.write_log(context, public_func.network_error);
+            public_func.send_fallback_sms(context, request_body.text, sub);
+            return;
+        }
+        String raw_request_body_text = request_body.text;
+        if (sharedPreferences.getBoolean("verification_code", false)) {
+            String verification = public_func.get_verification_code(message_body.toString());
+            if (verification != null) {
+                request_body.parse_mode = "html";
+                request_body.text = request_body.text
+                        .replace("<", "&lt;")
+                        .replace(">", "&gt;")
+                        .replace("&", "&amp;")
+                        .replace(verification, "<code>" + verification + "</code>");
             }
-            String raw_request_body_text = request_body.text;
-            if (sharedPreferences.getBoolean("verification_code", false)) {
-                String verification = public_func.get_verification_code(message_body.toString());
-                if (verification != null) {
-                    request_body.parse_mode = "html";
-                    request_body.text = request_body.text
-                            .replace("<", "&lt;")
-                            .replace(">", "&gt;")
-                            .replace("&", "&amp;")
-                            .replace(verification, "<code>" + verification + "</code>");
-                }
+        }
+        RequestBody body = RequestBody.create(public_func.JSON, new Gson().toJson(request_body));
+        OkHttpClient okhttp_client = public_func.get_okhttp_obj(sharedPreferences.getBoolean("doh_switch", true));
+        Request request = new Request.Builder().url(request_uri).method("POST", body).build();
+        Call call = okhttp_client.newCall(request);
+        final String error_head = "Send SMS forward failed:";
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                String error_message = error_head + e.getMessage();
+                public_func.write_log(context, error_message);
+                public_func.send_fallback_sms(context, raw_request_body_text, sub);
             }
-            RequestBody body = RequestBody.create(public_func.JSON, new Gson().toJson(request_body));
-            OkHttpClient okhttp_client = public_func.get_okhttp_obj(sharedPreferences.getBoolean("doh_switch", true));
-            Request request = new Request.Builder().url(request_uri).method("POST", body).build();
-            Call call = okhttp_client.newCall(request);
-            final String error_head = "Send SMS forward failed:";
-            call.enqueue(new Callback() {
-                @Override
-                public void onFailure(@NonNull Call call, @NonNull IOException e) {
-                    String error_message = error_head + e.getMessage();
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.code() != 200) {
+                    assert response.body() != null;
+                    String error_message = error_head + response.code() + " " + response.body().string();
                     public_func.write_log(context, error_message);
                     public_func.send_fallback_sms(context, raw_request_body_text, sub);
                 }
-                @Override
-                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                    if (response.code() != 200) {
-                        assert response.body() != null;
-                        String error_message = error_head + response.code() + " " + response.body().string();
-                        public_func.write_log(context, error_message);
-                        public_func.send_fallback_sms(context, raw_request_body_text, sub);
-                    }
-                    if (response.code() == 200) {
-                        assert response.body() != null;
-                        if (public_func.is_numeric(message_address)) {
-                            public_func.add_message_list(context, public_func.get_message_id(response.body().string()), message_address, slot, sub);
-                        }
+                if (response.code() == 200) {
+                    assert response.body() != null;
+                    if (public_func.is_numeric(message_address)) {
+                        public_func.add_message_list(context, public_func.get_message_id(response.body().string()), message_address, slot, sub);
                     }
                 }
-            });
-        }
+            }
+        });
     }
 }
+
 
