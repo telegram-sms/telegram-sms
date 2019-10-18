8b0e15fd96158f12e6df98a0b35eca1bae36c543
qwe7002 <qwe7002@hotmail.com>
2019-10-18 18:04:53 +0800

    修复无网络状态下

--- app/src/main/java/com/qwe7002/telegram_sms/sms_receiver.java
+++ app/src/main/java/com/qwe7002/telegram_sms/sms_receiver.java
@@ -155,7 +155,7 @@ public class sms_receiver extends BroadcastReceiver {
         }
         if (!public_func.check_network_status(context)) {
             public_func.write_log(context, public_func.network_error);
-            public_func.send_fallback_sms(context, request_body.text, sub);
+            public_func.send_fallback_sms(context, raw_request_body_text, sub);
             return;
         }
 
