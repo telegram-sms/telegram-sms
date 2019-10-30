7c0ef28a50bcc2d9c00834e9458cf2f937987f38
qwe7002 <qwe7002@hotmail.com>
2019-10-30 18:09:35 +0800

    修改日志输出的位置

--- app/src/main/java/com/qwe7002/telegram_sms/boot_receiver.java
+++ app/src/main/java/com/qwe7002/telegram_sms/boot_receiver.java
@@ -11,9 +11,9 @@ public class boot_receiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         Log.d("boot_receiver", "Receive action: "+intent.getAction());
-        public_func.write_log(context, "Received [" + intent.getAction() + "] broadcast, starting background service.");
         final SharedPreferences sharedPreferences = context.getSharedPreferences("data", Context.MODE_PRIVATE);
         if (sharedPreferences.getBoolean("initialized", false)) {
+            public_func.write_log(context, "Received [" + intent.getAction() + "] broadcast, starting background service.");
             public_func.start_service(context, sharedPreferences.getBoolean("battery_monitoring_switch", false), sharedPreferences.getBoolean("chat_command", false));
         }
     }
