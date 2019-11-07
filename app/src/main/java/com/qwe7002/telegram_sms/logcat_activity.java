a0798886ea5a7a3898e383af1275cdb0d858cd6d
qwe7002 <qwe7002@hotmail.com>
2019-11-07 16:55:36 +0800

    移除无用函数

--- app/src/main/java/com/qwe7002/telegram_sms/logcat_activity.java
+++ app/src/main/java/com/qwe7002/telegram_sms/logcat_activity.java
@@ -10,8 +10,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import io.paperdb.Paper;
-
 public class logcat_activity extends AppCompatActivity {
 
     private Context context;
