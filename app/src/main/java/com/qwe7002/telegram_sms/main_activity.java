61bd8b362269a9cd8a8989ea685926029f25f36a
qwe7002 <qwe7002@hotmail.com>
2020-03-21 18:28:15 +0800

    移除多余数据项

--- app/src/main/java/com/qwe7002/telegram_sms/main_activity.java
+++ app/src/main/java/com/qwe7002/telegram_sms/main_activity.java
@@ -419,7 +419,6 @@ public class main_activity extends AppCompatActivity {
                     editor.putBoolean("doh_switch", doh_switch.isChecked());
                     editor.putBoolean("privacy_mode", privacy_mode_switch.isChecked());
                     editor.putBoolean("initialized", true);
-                    editor.putBoolean("conversion_data_structure", true);
                     editor.putBoolean("privacy_dialog_agree", true);
                     editor.apply();
                     new Thread(() -> {
