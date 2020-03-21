d76fb762a3aad452f5e7b8f24666d13cc4a9f4b5
qwe7002 <qwe7002@hotmail.com>
2020-03-21 18:42:17 +0800

    修改命令列表判断

--- app/src/main/java/com/qwe7002/telegram_sms/chat_command_service.java
+++ app/src/main/java/com/qwe7002/telegram_sms/chat_command_service.java
@@ -209,9 +209,7 @@ public class chat_command_service extends Service {
                 if (public_func.get_active_card(context) == 2) {
                     sms_command = getString(R.string.sendsms_dual);
                 }
-                if (Paper.book().read("black_keyword_list", new ArrayList<>()).size() != 0) {
-                    sms_command += "\n" + getString(R.string.get_spam_sms);
-                }
+                sms_command += "\n" + getString(R.string.get_spam_sms);
                 if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
                     if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
                         ussd_command = "\n" + getString(R.string.send_ussd_command);
