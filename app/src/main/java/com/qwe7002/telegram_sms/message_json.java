abea382bb7663a87447698f62461d2e3eb3e8a99
qwe7002 <qwe7002@hotmail.com>
2020-06-25 23:14:18 +0800

    添加注释

--- app/src/main/java/com/qwe7002/telegram_sms/message_json.java
+++ app/src/main/java/com/qwe7002/telegram_sms/message_json.java
@@ -1,10 +1,11 @@
 package com.qwe7002.telegram_sms;
 
 class message_json {
-    @SuppressWarnings({"unused", "RedundantSuppression"})
-    final boolean disable_web_page_preview = true;
     long message_id;
     String parse_mode;
     String chat_id;
     String text;
+    //Turn off page preview to avoid being tracked
+    @SuppressWarnings({"unused", "RedundantSuppression"})
+    final boolean disable_web_page_preview = true;
 }
