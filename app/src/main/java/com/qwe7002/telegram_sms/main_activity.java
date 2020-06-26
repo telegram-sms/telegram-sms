71fb5595da8bf1ade7fff3776f883964077772c1
qwe7002 <qwe7002@hotmail.com>
2020-06-26 22:42:19 +0800

    删除无用函数

--- app/src/main/java/com/qwe7002/telegram_sms/main_activity.java
+++ app/src/main/java/com/qwe7002/telegram_sms/main_activity.java
@@ -72,7 +72,6 @@ public class main_activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = getApplicationContext();
-        Log.i(TAG, "Current API address: " + public_func.get_url("", ""));
 
         final EditText chat_id = findViewById(R.id.chat_id);
         final EditText bot_token = findViewById(R.id.bot_token);
