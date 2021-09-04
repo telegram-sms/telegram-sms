f05d2cbd6a58aadff2dfb07be7158e22be308a91
qwe7002 <qwe7002@Hotmail.com>
2021-09-04 14:16:05 -0400

    更新依赖，更换扫码库

--- app/src/main/java/com/qwe7002/telegram_sms/scanner_activity.java
+++ app/src/main/java/com/qwe7002/telegram_sms/scanner_activity.java
@@ -6,23 +6,20 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
-import com.google.zxing.Result;
 import com.qwe7002.telegram_sms.value.const_value;
 
-import org.jetbrains.annotations.NotNull;
 
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
+public class scanner_activity extends Activity {
 
-public class scanner_activity extends Activity implements ZXingScannerView.ResultHandler {
-    private ZXingScannerView scanner_view;
-
+    private CodeScanner mCodeScanner;
     @Override
     public void onCreate(Bundle state) {
         super.onCreate(state);
@@ -30,36 +27,33 @@ public class scanner_activity extends Activity implements ZXingScannerView.Resul
         Toolbar toolbar = findViewById(R.id.scan_toolbar);
         toolbar.setTitle(R.string.scan_title);
         toolbar.setTitleTextColor(Color.WHITE);
-        ViewGroup contentFrame = findViewById(R.id.content_frame);
-        scanner_view = new ZXingScannerView(this);
-        contentFrame.addView(scanner_view);
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(result -> runOnUiThread(() -> {
+            String TAG = "activity_scanner";
+            Log.d(TAG, "format: " + result.getBarcodeFormat().toString() + " content: " + result.getText());
+            if (!json_validate(result.getText())) {
+                Toast.makeText(scanner_activity.this, "The QR code is not legal", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Intent intent = new Intent().putExtra("config_json", result.getText());
+            setResult(const_value.RESULT_CONFIG_JSON, intent);
+            finish();
+        }));
+        scannerView.setOnClickListener(view -> mCodeScanner.startPreview());
     }
 
 
     @Override
     public void onResume() {
-        scanner_view.setResultHandler(this);
-        scanner_view.startCamera();
         super.onResume();
+        mCodeScanner.startPreview();
     }
 
     @Override
     public void onPause() {
-        scanner_view.stopCamera();
+        mCodeScanner.releaseResources();
         super.onPause();
-    }
-
-    @Override
-    public void handleResult(@NotNull Result rawResult) {
-        String TAG = "activity_scanner";
-        Log.d(TAG, "format: " + rawResult.getBarcodeFormat().toString() + " content: " + rawResult.getText());
-        if (!json_validate(rawResult.getText())) {
-            Toast.makeText(this, "The QR code is not legal", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        Intent intent = new Intent().putExtra("config_json", rawResult.getText());
-        setResult(const_value.RESULT_CONFIG_JSON, intent);
-        finish();
     }
 
     boolean json_validate(String jsonStr) {
