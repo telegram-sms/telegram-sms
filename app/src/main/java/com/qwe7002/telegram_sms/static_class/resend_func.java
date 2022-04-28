0c527258e5a021a5b6769b674d1be4831eaee94e
qwe7002 <qwe7002@Hotmail.com>
2022-04-28 07:19:54 -0400

    修改自動重發服務的實現，添加時間選項

--- app/src/main/java/com/qwe7002/telegram_sms/static_class/resend_func.java
+++ app/src/main/java/com/qwe7002/telegram_sms/static_class/resend_func.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 
+import com.qwe7002.telegram_sms.R;
 import com.qwe7002.telegram_sms.resend_service;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 import io.paperdb.Paper;
 
@@ -16,6 +20,8 @@ resend_func {
         ArrayList<String> resend_list;
         Paper.init(context);
         resend_list = Paper.book().read("resend_list", new ArrayList<>());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(context.getString(R.string.time_format), Locale.UK);
+        message += "\n"+context.getString(R.string.time) + simpleDateFormat.format(new Date(System.currentTimeMillis()));
         resend_list.add(message);
         Paper.book().write("resend_list", resend_list);
         start_resend(context);
