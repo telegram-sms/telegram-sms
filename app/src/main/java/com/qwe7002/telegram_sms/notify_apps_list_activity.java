9ead7f496e65629ca48cc6f76b748dfcc5380752
qwe7002 <qwe7002@Hotmail.com>
2023-01-08 09:00:17 -0500

    更新依賴版本實現

--- app/src/main/java/com/qwe7002/telegram_sms/notify_apps_list_activity.java
+++ app/src/main/java/com/qwe7002/telegram_sms/notify_apps_list_activity.java
@@ -30,32 +30,32 @@ import java.util.List;
 import io.paperdb.Paper;
 
 public class notify_apps_list_activity extends AppCompatActivity {
-    private app_adapter app_adapter;
+    private notify_apps_list_activity.appAdapter appAdapter;
     private Context context;
 
     @NotNull
-    private List<app_info> scan_app_list(PackageManager packageManager) {
-        List<app_info> app_info_list = new ArrayList<>();
+    private List<appInfo> scanAppList(PackageManager packageManager) {
+        List<appInfo> appInfoList = new ArrayList<>();
         try {
-            List<PackageInfo> package_info_list = packageManager.getInstalledPackages(0);
-            for (int i = 0; i < package_info_list.size(); i++) {
-                PackageInfo package_info = package_info_list.get(i);
-                if (package_info.packageName.equals(context.getPackageName())) {
+            List<PackageInfo> packageInfoList = packageManager.getInstalledPackages(0);
+            for (int i = 0; i < packageInfoList.size(); i++) {
+                PackageInfo packageInfo = packageInfoList.get(i);
+                if (packageInfo.packageName.equals(context.getPackageName())) {
                     continue;
                 }
-                app_info app_info = new app_info();
-                app_info.package_name = package_info.packageName;
-                app_info.app_name = package_info.applicationInfo.loadLabel(packageManager).toString();
-                if (package_info.applicationInfo.loadIcon(packageManager) == null) {
+                appInfo info = new appInfo();
+                info.packageName = packageInfo.packageName;
+                info.appName = packageInfo.applicationInfo.loadLabel(packageManager).toString();
+                if (packageInfo.applicationInfo.loadIcon(packageManager) == null) {
                     continue;
                 }
-                app_info.app_icon = package_info.applicationInfo.loadIcon(packageManager);
-                app_info_list.add(app_info);
+                info.appIcon = packageInfo.applicationInfo.loadIcon(packageManager);
+                appInfoList.add(info);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return app_info_list;
+        return appInfoList;
     }
 
     @Override
@@ -65,12 +65,12 @@ public class notify_apps_list_activity extends AppCompatActivity {
         Paper.init(context);
         this.setTitle(getString(R.string.app_list));
         setContentView(R.layout.activity_notify_apps_list);
-        final ListView app_list = findViewById(R.id.app_listview);
-        final SearchView filter_edit = findViewById(R.id.filter_searchview);
-        filter_edit.setIconifiedByDefault(false);
-        app_list.setTextFilterEnabled(true);
-        app_adapter = new app_adapter(context);
-        filter_edit.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        final ListView appList = findViewById(R.id.app_listview);
+        final SearchView filterEdit = findViewById(R.id.filter_searchview);
+        filterEdit.setIconifiedByDefault(false);
+        appList.setTextFilterEnabled(true);
+        appAdapter = new appAdapter(context);
+        filterEdit.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 return true;
@@ -78,37 +78,37 @@ public class notify_apps_list_activity extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                app_adapter.getFilter().filter(newText);
+                appAdapter.getFilter().filter(newText);
                 return false;
             }
         });
 
-        app_list.setAdapter(app_adapter);
+        appList.setAdapter(appAdapter);
         new Thread(() -> {
-            final List<app_info> app_info_list = scan_app_list(notify_apps_list_activity.this.getPackageManager());
+            final List<appInfo> appInfoList = scanAppList(notify_apps_list_activity.this.getPackageManager());
             runOnUiThread(() -> {
-                ProgressBar scan_label = findViewById(R.id.progress_view);
-                scan_label.setVisibility(View.GONE);
-                app_adapter.setData(app_info_list);
+                ProgressBar progressBar = findViewById(R.id.progress_view);
+                progressBar.setVisibility(View.GONE);
+                appAdapter.setData(appInfoList);
             });
         }).start();
     }
 
-    static class app_adapter extends BaseAdapter implements Filterable {
+    static class appAdapter extends BaseAdapter implements Filterable {
         final String TAG = "notify_activity";
-        List<String> listen_list;
-        List<app_info> app_info_list = new ArrayList<>();
-        List<app_info> view_app_info_list = new ArrayList<>();
+        List<String> listenList;
+        List<appInfo> appInfoList = new ArrayList<>();
+        List<appInfo> viewAppInfoList = new ArrayList<>();
         private final Context context;
         private final Filter filter = new Filter() {
             @NotNull
             @Override
             protected FilterResults performFiltering(CharSequence constraint) {
                 FilterResults results = new FilterResults();
-                List<app_info> list = new ArrayList<>();
-                for (app_info app_info_item : app_info_list) {
-                    if (app_info_item.app_name.toLowerCase().contains(constraint.toString().toLowerCase())) {
-                        list.add(app_info_item);
+                List<appInfo> list = new ArrayList<>();
+                for (appInfo appInfoItem : appInfoList) {
+                    if (appInfoItem.appName.toLowerCase().contains(constraint.toString().toLowerCase())) {
+                        list.add(appInfoItem);
                     }
                 }
                 results.values = list;
@@ -119,38 +119,38 @@ public class notify_apps_list_activity extends AppCompatActivity {
             @SuppressWarnings("unchecked")
             @Override
             protected void publishResults(CharSequence constraint, @NotNull FilterResults results) {
-                view_app_info_list = (ArrayList<app_info>) results.values;
+                viewAppInfoList = (ArrayList<appInfo>) results.values;
                 notifyDataSetChanged();
             }
         };
 
-        app_adapter(Context context) {
+        appAdapter(Context context) {
             this.context = context;
-            this.listen_list = Paper.book("system_config").read("notify_listen_list", new ArrayList<>());
+            this.listenList = Paper.book("system_config").read("notify_listen_list", new ArrayList<>());
         }
 
-        public List<app_info> getData() {
-            return app_info_list;
+        public List<appInfo> getData() {
+            return appInfoList;
         }
 
-        public void setData(List<app_info> apps_info_list) {
-            this.app_info_list = apps_info_list;
-            this.view_app_info_list = app_info_list;
+        public void setData(List<appInfo> appInfoList) {
+            this.appInfoList = appInfoList;
+            this.viewAppInfoList = this.appInfoList;
             notifyDataSetChanged();
         }
 
         @Override
         public int getCount() {
-            if (view_app_info_list != null && view_app_info_list.size() > 0) {
-                return view_app_info_list.size();
+            if (viewAppInfoList != null && viewAppInfoList.size() > 0) {
+                return viewAppInfoList.size();
             }
             return 0;
         }
 
         @Override
         public Object getItem(int position) {
-            if (view_app_info_list != null && view_app_info_list.size() > 0) {
-                return view_app_info_list.get(position);
+            if (viewAppInfoList != null && viewAppInfoList.size() > 0) {
+                return viewAppInfoList.get(position);
             }
             return null;
         }
@@ -161,40 +161,42 @@ public class notify_apps_list_activity extends AppCompatActivity {
         }
 
         @Override
-        public View getView(int position, View convert_view, ViewGroup parent) {
-            view_holder view_holder_object;
-            app_info app_info = view_app_info_list.get(position);
-            if (convert_view == null) {
-                view_holder_object = new view_holder();
-                convert_view = LayoutInflater.from(context).inflate(R.layout.item_app_info, parent, false);
-                view_holder_object.app_icon = convert_view.findViewById(R.id.app_icon_imageview);
-                view_holder_object.package_name = convert_view.findViewById(R.id.package_name_textview);
-                view_holder_object.app_name = convert_view.findViewById(R.id.app_name_textview);
-                view_holder_object.app_checkbox = convert_view.findViewById(R.id.select_checkbox);
-                convert_view.setTag(view_holder_object);
+        public View getView(int position, View convertView, ViewGroup parent) {
+            viewHolder viewHolderObj;
+            appInfo appInfoObj = viewAppInfoList.get(position);
+            if (convertView == null) {
+                viewHolderObj = new viewHolder();
+                convertView = LayoutInflater.from(context).inflate(R.layout.item_app_info, parent, false);
+                viewHolderObj.appIcon = convertView.findViewById(R.id.app_icon_imageview);
+                viewHolderObj.packageName = convertView.findViewById(R.id.package_name_textview);
+                viewHolderObj.appName = convertView.findViewById(R.id.app_name_textview);
+                viewHolderObj.appCheckbox = convertView.findViewById(R.id.select_checkbox);
+                convertView.setTag(viewHolderObj);
             } else {
-                view_holder_object = (notify_apps_list_activity.app_adapter.view_holder) convert_view.getTag();
+                viewHolderObj = (viewHolder) convertView.getTag();
             }
-            view_holder_object.app_icon.setImageDrawable(app_info.app_icon);
-            view_holder_object.app_name.setText(app_info.app_name);
-            view_holder_object.package_name.setText(app_info.package_name);
-            view_holder_object.app_checkbox.setChecked(listen_list.contains(app_info.package_name));
-            view_holder_object.app_checkbox.setOnClickListener(v -> {
-                app_info item_info = (app_info) getItem(position);
-                String package_name = item_info.package_name;
-                List<String> listen_list_temp = Paper.book("system_config").read("notify_listen_list", new ArrayList<>());
-                if (view_holder_object.app_checkbox.isChecked()) {
-                    if (!listen_list_temp.contains(package_name)) {
-                        listen_list_temp.add(package_name);
+            viewHolderObj.appIcon.setImageDrawable(appInfoObj.appIcon);
+            viewHolderObj.appName.setText(appInfoObj.appName);
+            viewHolderObj.packageName.setText(appInfoObj.packageName);
+            viewHolderObj.appCheckbox.setChecked(listenList.contains(appInfoObj.packageName));
+            viewHolderObj.appCheckbox.setOnClickListener(v -> {
+                appInfo itemInfo = (appInfo) getItem(position);
+                String packageName = itemInfo.packageName;
+                List<String> listenListTemp = Paper.book("system_config").read("notify_listen_list", new ArrayList<>());
+                if (viewHolderObj.appCheckbox.isChecked()) {
+                    assert listenListTemp != null;
+                    if (!listenListTemp.contains(packageName)) {
+                        listenListTemp.add(packageName);
                     }
                 } else {
-                    listen_list_temp.remove(package_name);
+                    assert listenListTemp != null;
+                    listenListTemp.remove(packageName);
                 }
-                Log.d(TAG, "notify_listen_list: " + listen_list_temp);
-                Paper.book("system_config").write("notify_listen_list", listen_list_temp);
-                listen_list = listen_list_temp;
+                Log.d(TAG, "notify_listen_list: " + listenListTemp);
+                Paper.book("system_config").write("notify_listen_list", listenListTemp);
+                listenList = listenListTemp;
             });
-            return convert_view;
+            return convertView;
         }
 
         @Override
@@ -202,18 +204,18 @@ public class notify_apps_list_activity extends AppCompatActivity {
             return filter;
         }
 
-        static class view_holder {
-            ImageView app_icon;
-            TextView app_name;
-            TextView package_name;
-            CheckBox app_checkbox;
+        static class viewHolder {
+            ImageView appIcon;
+            TextView appName;
+            TextView packageName;
+            CheckBox appCheckbox;
         }
 
     }
 
-    static class app_info {
-        Drawable app_icon;
-        String package_name;
-        String app_name;
+    static class appInfo {
+        Drawable appIcon;
+        String packageName;
+        String appName;
     }
 }
