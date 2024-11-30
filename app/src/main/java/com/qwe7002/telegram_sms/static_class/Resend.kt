58530a89b5da986c6460a6f3818c28a95a56bc3d
qwe7002-dev <qwe7002.dev@gmail.com>
2024-11-30 18:55:00 +0800

    Add resend service to pull up

--- app/src/main/java/com/qwe7002/telegram_sms/static_class/Resend.kt
+++ app/src/main/java/com/qwe7002/telegram_sms/static_class/Resend.kt
@@ -2,6 +2,7 @@ package com.qwe7002.telegram_sms.static_class
 
 import android.content.Context
 import com.qwe7002.telegram_sms.R
+import com.qwe7002.telegram_sms.ReSendJob
 import io.paperdb.Paper
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -18,5 +19,6 @@ object Resend {
         checkNotNull(resendList)
         resendList.add(message)
         Paper.book().write("resend_list", resendList)
+        ReSendJob.startJob(context)
     }
 }
