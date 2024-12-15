c583ad9796b6723e06137acc911b3ade6b3abeb1
qwe7002-dev <qwe7002.dev@gmail.com>
2024-12-15 11:02:56 +0800

    Add title information

--- app/src/main/java/com/qwe7002/telegram_sms/NotificationService.kt
+++ app/src/main/java/com/qwe7002/telegram_sms/NotificationService.kt
@@ -78,7 +78,7 @@ class NotificationService : NotificationListenerService() {
         requestBody.chatId = chatId.toString()
         requestBody.messageThreadId = messageThreadId.toString()
         requestBody.text = Template.render( applicationContext, "TPL_notification", mapOf("APP" to appName, "Title" to title, "Description" to content))
-        CCSendJob.startJob(applicationContext, requestBody.text)
+        CCSendJob.startJob(applicationContext,applicationContext.getString(R.string.Notification_Listener_title), requestBody.text)
         val body: RequestBody = Gson().toJson(requestBody).toRequestBody(constValue.JSON)
         val okhttpObj = Network.getOkhttpObj(
             sharedPreferences.getBoolean("doh_switch", true),
