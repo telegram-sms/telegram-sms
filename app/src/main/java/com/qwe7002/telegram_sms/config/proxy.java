bee118ed33135e2dca7f14328b6693f94ffd527c
PAnkThakur <159337989+pankj2726@users.noreply.github.com>
2025-05-30 08:38:23 +0530

    Update proxy.java

--- app/src/main/java/com/qwe7002/telegram_sms/config/proxy.java
+++ app/src/main/java/com/qwe7002/telegram_sms/config/proxy.java
@@ -1,10 +1,49 @@
 package com.qwe7002.telegram_sms.config;
 
 public class proxy {
-    public int port = 1080;
-    public String host = "";
-    public String username = "";
-    public String password = "";
-    public boolean enable = false;
-    public boolean dns_over_socks5 = true;
+    private String host = "";
+    private int port = 0;
+    private String username = "";
+    private String password = "";
+    private String type = "HTTP";
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
