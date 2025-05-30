141e46c78b2328d88ccfd4b783ae8ca9fcdbe310
PAnkThakur <159337989+pankj2726@users.noreply.github.com>
2025-05-30 08:37:29 +0530

    Update log.java

--- app/src/main/java/com/qwe7002/telegram_sms/static_class/log.java
+++ app/src/main/java/com/qwe7002/telegram_sms/static_class/log.java
@@ -3,101 +3,35 @@ package com.qwe7002.telegram_sms.static_class;
 import android.content.Context;
 import android.util.Log;
 
-import com.qwe7002.telegram_sms.R;
-
-import org.jetbrains.annotations.NotNull;
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import io.paperdb.Paper;
-
 public class log {
-    public static void writeLog(@NotNull Context context, String log) {
-        Log.i("write_log", log);
-        int new_file_mode = Context.MODE_APPEND;
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(context.getString(R.string.time_format), Locale.UK);
-        String write_string = "\n" + simpleDateFormat.format(new Date(System.currentTimeMillis())) + " " + log;
-        int log_count = Paper.book("system_config").read("log_count", 0);
-        if (log_count >= 50000) {
-            resetLogFile(context);
-        }
-        Paper.book("system_config").write("log_count", ++log_count);
-        writeLogFile(context, write_string, new_file_mode);
-    }
+    private static final String TAG = "log";
+    private static final String LOG_DIRECTORY = "log";
+    private static final String LOG_FILE_PREFIX = "error_log_";
+    private static final String LOG_FILE_SUFFIX = ".log";
 
-    public static String readLog(@NotNull Context context, int line) {
-        String result = context.getString(R.string.no_logs);
-        String TAG = "read_file_last_line";
-        StringBuilder builder = new StringBuilder();
-        FileInputStream file_stream = null;
-        FileChannel channel = null;
+    public static void writeLog(Context context, String log_message) {
+        Log.i(TAG, log_message);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.UK);
+        String write_log = simpleDateFormat.format(new Date()) + " " + log_message + "\n";
+        File log_file = new File(context.getExternalFilesDir(LOG_DIRECTORY), getLogFileName());
         try {
-            file_stream = context.openFileInput("error.log");
-            channel = file_stream.getChannel();
-            ByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
-            buffer.position((int) channel.size());
-            int count = 0;
-            for (long i = channel.size() - 1; i >= 0; i--) {
-                char c = (char) buffer.get((int) i);
-                builder.insert(0, c);
-                if (c == '\n') {
-                    if (count == (line - 1)) {
-                        break;
-                    }
-                    ++count;
-                }
-            }
-            if (!builder.toString().isEmpty()) {
-                return builder.toString();
-            } else {
-                return result;
-            }
+            FileWriter fileWriter = new FileWriter(log_file, true);
+            fileWriter.write(write_log);
+            fileWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
-            Log.d(TAG, "Unable to read the file.");
-            return result;
-        } finally {
-            try {
-                if (file_stream != null) {
-                    file_stream.close();
-                }
-                if (channel != null) {
-                    channel.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 
-    public static void resetLogFile(Context context) {
-        Paper.book("system_config").delete("log_count");
-        writeLogFile(context, "", Context.MODE_PRIVATE);
-    }
-
-    private static void writeLogFile(@NotNull Context context, @NotNull String write_string, int mode) {
-        FileOutputStream file_stream = null;
-        try {
-            file_stream = context.openFileOutput("error.log", mode);
-            byte[] bytes = write_string.getBytes();
-            file_stream.write(bytes);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (file_stream != null) {
-                try {
-                    file_stream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+    private static String getLogFileName() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.UK);
+        return LOG_FILE_PREFIX + simpleDateFormat.format(new Date()) + LOG_FILE_SUFFIX;
     }
 }
