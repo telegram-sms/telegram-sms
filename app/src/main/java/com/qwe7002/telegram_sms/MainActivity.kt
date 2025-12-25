efad5236cc52509961802267a7de2bc32593cf80
qwe7002 <qwe7002@qwe7002.dev>
2025-12-25 02:57:21 -0800

    chore(main): suppress GestureBackNavigation lint warning and remove commented proxy code

--- app/src/main/java/com/qwe7002/telegram_sms/MainActivity.kt
+++ app/src/main/java/com/qwe7002/telegram_sms/MainActivity.kt
@@ -67,7 +67,7 @@ class MainActivity : AppCompatActivity() {
     private val gson = Gson()
     private var setPermissionBack = false
 
-    @SuppressLint("BatteryLife", "UseKtx")
+    @SuppressLint("BatteryLife", "UseKtx", "GestureBackNavigation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -197,12 +197,6 @@ class MainActivity : AppCompatActivity() {
         verificationCodeSwitch.isChecked = preferences.getBoolean("verification_code", false)
 
         dohSwitch.isChecked = preferences.getBoolean("doh_switch", true)
-/*        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            val proxyMMKV = MMKV.mmkvWithID("proxy")
-            dohSwitch.isEnabled = proxyMMKV.getBoolean("enable", false)
-            *//*            dohSwitch.isEnabled =
-                            !Paper.book("system_config").read("proxy_config", proxy())!!.enable*//*
-        }*/
 
 
         chatIdEditView.addTextChangedListener(object : TextWatcher {
