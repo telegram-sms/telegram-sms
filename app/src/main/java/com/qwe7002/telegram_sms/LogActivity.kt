3a22fbf9e6fd829a6ca7d9ddfd801596d822b859
qwe7002 <git@qwe7002.dev>
2026-01-19 06:07:12 -0800

    refactor: remove unused Menu and MenuItem imports from LogActivity

--- app/src/main/java/com/qwe7002/telegram_sms/LogActivity.kt
+++ app/src/main/java/com/qwe7002/telegram_sms/LogActivity.kt
@@ -6,8 +6,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
